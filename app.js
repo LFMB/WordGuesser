f0596723ca8e70807fb109ba9db59efe5dd64aff
LFMB <lucas.blom@maine.edu>
2022-02-23 11:58:02 -0500

    transformed function declarations into arrow functions in eventual move to class structure + added state to track # of guesses

--- app.js
+++ app.js
@@ -5,11 +5,33 @@ const secret_words = ['hello', 'sheep', 'bling', 'sleep']
 
 // Thinking of turning WordGuesser into a class
 // with word_info becoming this in the constructor and taking secret_words as the param for init
+/*
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  // Method
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 10);
+
+console.log(square.area); // 100
+
+*/
 
 // initial state 
 const word_info = {
   selected_word: '',
-  number_guesses: 0,
+  current_guess: '',
+  guess_count: 0,
 }
 
 // get word of day
@@ -33,16 +55,33 @@ const randomWordPicker = (word_list) => {
 
 // get guess
 // will need to create a UI for this?
+// going to manually create guesses for now at bottom of app
+const setGuess = (guess) => {
+    // update guess count
+    word_info.guess_count =  word_info.guess_count + 1
+    word_info.current_guess = guess
+
+    // not happy about the nesting of checkGuess
+    // maybe create an async/await?
+    checkGuess(guess)
+}
 
 // check guess
-function checkGuess(guess, solution){
-  // update guess count
+// made into arrow function to prep for eventual transition to a class format
+const checkGuess = (guess) => {
+  console.log('checkGuess guess: ', guess)
 
   // check guess against solution
 }
 
 // provide feed back
 
+// should create constructor and set assignment there
 randomWordPicker(secret_words)
+
+// currently calls checkGuess in function
+setGuess('joy')
+
+setGuess('joyful')
 
 console.log(word_info)
