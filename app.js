a809ab4f7f3cadd73694ee07426dcaf1272e6f71
LFMB <lucas.blom@maine.edu>
2022-02-23 10:38:00 -0500

    finished MVP for randomWordPicker function

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 
-// word bank - likely will update
+// word bank - likely will turn into a more complicated data structure
+// heck maybe its own app crawling twitter for word ideas?
 const secret_words = ['hello', 'sheep', 'bling', 'sleep']
 
-// get word of day
+// initial state 
+const word_info = {
+  selected_word: '',
+  number_guesses: 0,
+}
 
+// get word of day
+// assuming array for type in parameter
+function randomWordPicker(word_list){
+  // randomly pick a number between 0 and 1 less the index length
+  const max_options = word_list.length
+  
+  // use random selection to access key from array
+  // assumes min is 0 so no need to do Math.floor(Math.random() * (max - min) + min)
+  // the floor should handle the issue in the difference between length and index
+  const rand_index = Math.floor(Math.random() * max_options)
+
+  // some day add state that tracks if index/ word was already selected
+  const selected_word = word_list[rand_index]
+
+  // console.log('selected_word: ', selected_word)
+  return selected_word;
+
+}
 
 // get guess
+// will need to create a UI for this?
 
 // check guess
+function checkGuess(guess, solution){
+  // update guess count
+
+  // check guess against solution
+}
 
 // provide feed back
 
+word_info.selected_word = randomWordPicker(secret_words)
 
-
-
-console.log(secret_words)
+// console.log(word_info)
