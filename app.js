e6425ed65755d10e1de493f50a8d16ab1016fcfe
LFMB <lucas.blom@maine.edu>
2022-02-22 19:06:10 -0500

    started wordguesser app

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// word bank - likely will update
+const secret_words = ['hello', 'sheep', 'bling', 'sleep']
+
+// get word of day
+
+
+// get guess
+
+// check guess
+
+// provide feed back
+
+
+
+
+console.log(secret_words)
