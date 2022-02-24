ad77f82a14bb94f923f922e4b8049520c8229680
LFMB <lucas.blom@maine.edu>
2022-02-23 19:11:45 -0500

    have rough checkGuess done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 // word bank - likely will turn into a more complicated data structure
 // heck, maybe its own app crawling twitter for word ideas?
-const secret_words = ['hello', 'sheep', 'bling', 'sleep']
+// const secret_words = ['hello', 'sheep', 'bling', 'sleep']
+const secret_words = ['hello']
 
 // Thinking of turning WordGuesser into a class
 // with word_info becoming this in the constructor and taking secret_words as the param for init
@@ -31,11 +32,13 @@ console.log(square.area); // 100
 // initial state 
 const word_info = {
   selected_word: '',
+  guess_count: 0,
+  max_guesses: 5,
+  status: 'open',
   // maybe turn into an array or empty obj
   current_guess: '',
   rough_guess: '',
   formatted_guess: '',
-  guess_count: 0,
 }
 
 // get word of day
@@ -61,23 +64,29 @@ const randomWordPicker = (word_list) => {
 // will need to create a UI for this?
 // going to manually create guesses for now at bottom of app
 const setGuess = (guess) => {
-    // standardized case for guess - probably make into util function
-    const cleaned_guess = guess.toLowerCase() 
-
     // update guess count
     word_info.guess_count =  word_info.guess_count + 1
 
-    // standardized case for guess?
-    word_info.rough_guess = guess
-    word_info.formatted_guess = cleaned_guess
+    if (word_info.guess_count <= word_info.max_guesses) {
+      // standardized case for guess - probably make into util function
+      const cleaned_guess = guess.toLowerCase()
+      // standardized case for guess?
+      word_info.rough_guess = guess
+      word_info.formatted_guess = cleaned_guess
 
-    // not happy about the nesting of checkGuess
-    // maybe create an async/await?
-    checkGuess()
+      // not happy about the nesting of checkGuess
+      // maybe create an async/await?
+      checkGuess()
+    
+    } else {
+      console.log(`Sorry! You're now over the limit of ${word_info.max_guesses} with ${word_info.guess_count} guesses`)
+      word_info.status = 'failed'
+    }
 }
 
 // check guess - uses shared scope to access word_info object
 // made into arrow function to prep for eventual transition to a class format
+// currently returns the word_info object
 const checkGuess = () => {
   
   const guess = word_info.formatted_guess
@@ -85,16 +94,35 @@ const checkGuess = () => {
 
   if(guess === correct_answer){
     console.log(`Congrats! Your guess: ${guess} matches the mystery word: ${correct_answer}`)
-  } else {
-    // console.log("It's time to see what letters match")
+    word_info.status = 'solved'
+
+  }  else {
     // destructuring strings into arrays for easier comparison 
     const guess_array = [...guess],
           answer_array = [...correct_answer];
 
-    console.log('guess_array: ', guess_array, ' answer_array: ', answer_array)
+    // looping through guess 'bonds'
+    // should produce one item in results array 
+    const results = guess_array.filter((guessed_letter, guess_index) => {
+      console.log('guess_index: ', guess_index)
+      let letter_in_answer = answer_array.find((letter_in_answer, answer_index) =>  {
+        if(guessed_letter === letter_in_answer){
+          console.log(`guessed_letter: ${guessed_letter} === letter_in_answer: ${letter_in_answer}` )
+          // considering this structure for none complete failures: 
+          // {match_type: partial | full, letter: letter, position: guess_index}
+          return guessed_letter 
+        }
+      }) 
+
+    })
+
+    console.log('results array: ', results)
+    console.log('results.length: ', results.length)
+
   }
 
-  // check guess against solution
+  return word_info
+
 }
 
 // provide feed back
@@ -109,7 +137,7 @@ randomWordPicker(secret_words)
 // sticking to 5 letter words for now
 // currently no controller logic to enforce guess input same length as correct answer
 // so manually making the assumption
-setGuess('bands')
+setGuess('bonds')
 setGuess('hello')
 
 console.log(word_info)
